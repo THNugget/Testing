56069f2d8ca18555f4d029eacc27cd8f5b215706
MUKILTEO\1725608 <talonlee44@gmail.com>
2026-01-09 09:16:46 -0800

    Enemy AI

--- Assets/Scripts/Enemies/EnemyHealthManager.cs
+++ Assets/Scripts/Enemies/EnemyHealthManager.cs
@@ -12,7 +12,7 @@ public class EnemyHealthManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -5,6 +5,10 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D rb;
     public Transform player;
     public float enemySpeed = 3f;
+    //private bool isChasing = true;
+    public CircleCollider2D radius;
+    private bool hasLineOfSight = false;
+    private bool isChasing = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -15,10 +19,60 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.linearVelocity = direction * enemySpeed;
-        direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        if (hasLineOfSight && isChasing)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = direction * enemySpeed;
+            direction.Normalize();
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
+
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            isChasing = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
+        if(ray.collider != null)
+        {
+            
+            //if(ray.collider.gameobject.layer == layermask.nametolayer("player"))
+            //{
+            //    haslineofsight = true;
+            //}
+            //else
+            //{
+            //    haslineofsight = false;
+            //}
+            hasLineOfSight = ray.collider.CompareTag("Player");
+            if (hasLineOfSight)
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+            }
+        }
     }
 }
